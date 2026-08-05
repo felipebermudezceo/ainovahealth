62d58ec3160c18a8a9765768d13478de647ac8f2
felipebermudezceo <felipebermudezceo@gmail.com>
2026-08-04 19:54:03 -0500

    cambio de numero de whatsapp

--- components/ui/FloatingWhatsapp.tsx
+++ components/ui/FloatingWhatsapp.tsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export function FloatingWhatsapp() {
   return (
     <a
-      href="https://wa.me/573105769818"
+      href="https://wa.me/573118901570"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-[999]"
